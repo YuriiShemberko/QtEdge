5510beb2f36c90f1d01ccd0a30145e094cf56799
YuriiShemberko <yurii.shemberko@gmail.com>
2019-04-26 00:32:00 +0300

    Curtain draw in progress.

--- src/ui/QEdgeFrameView.cpp
+++ src/ui/QEdgeFrameView.cpp
@@ -8,15 +8,13 @@ QEdgeFrameView::QEdgeFrameView( QWidget *parent ) :
     m_frame_shown( false ),
     ui( new Ui::QEdgeFrameView ),
     m_current_frame( nullptr ),
-    m_image( nullptr )
+    m_image( new QImage( ":img/resources/curtain.png" ) )
 {
     ui->setupUi( this );
 }
 
 QEdgeFrameView::~QEdgeFrameView()
 {
-    av_frame_unref( m_current_frame );
-    av_frame_free( &m_current_frame );
     delete ui;
 }
 
@@ -50,51 +48,44 @@ void QEdgeFrameView::ResetImage( QImage *image )
 
 void QEdgeFrameView::paintEvent( QPaintEvent *ev )
 {
-    if( m_current_frame )
+    QPainter painter( this );
     {
-        QPainter painter( this );
+        if( m_current_frame )
         {
             std::lock_guard<std::mutex> frame_guard( m_frame_mtx );
             ResetImage( utils::AVFrameToQImage( m_current_frame, this->size() ) );
             Q_UNUSED( frame_guard );
         }
+    }
 
-        int w_diff = this->width() - m_image->width();
-        int h_diff = this->height() - m_image->height();
+    int w_diff = this->width() - m_image->width();
+    int h_diff = this->height() - m_image->height();
 
-        if( w_diff > h_diff  )
-        {
-            painter.fillRect( QRectF( 0, 0, w_diff / 2, this->height() ), Qt::black );
-            painter.drawImage( QPoint( w_diff / 2, 0 ), *m_image );
-            //painter.fillRect( QRectF( diff / 2, 0, m_image->width(), m_image->height()), Qt::red );
-            painter.fillRect( QRectF( w_diff / 2 + m_image->width(), 0, w_diff / 2, this->height() ), Qt::black );
-        }
+    if( w_diff > h_diff  )
+    {
+        painter.fillRect( QRectF( 0, 0, w_diff / 2, this->height() ), QColor( 192, 195, 193 ) );
+        painter.drawImage( QPoint( w_diff / 2, 0 ), *m_image );
+        painter.fillRect( QRectF( w_diff / 2 + m_image->width(), 0, w_diff / 2, this->height() ), QColor( 192, 195, 193 ) );
+    }
 
-        else if( h_diff > w_diff )
-        {
-            painter.fillRect( QRectF( 0, 0, this->width(), h_diff / 2 ), Qt::black );
-            painter.drawImage( 0, h_diff / 2, *m_image );
-            //painter.fillRect( QRectF( 0, diff / 2, m_image->width(), m_image->height()), Qt::red );
-            painter.fillRect( QRectF( 0, m_image->height() + h_diff / 2, this->width(), h_diff / 2 ), Qt::black );
-        }
-
-        else
-        {
-            painter.drawImage( QPoint( 0, 0 ), ( *m_image ) );
-        }
-
-        painter.end();
-
-        if( !m_frame_shown )
-        {
-            m_frame_shown = true;
-            emit frameShown();
-        }
+    else if( h_diff > w_diff )
+    {
+        painter.fillRect( QRectF( 0, 0, this->width(), h_diff / 2 ), QColor( 192, 195, 193 ) );
+        painter.drawImage( 0, h_diff / 2, *m_image );
+        painter.fillRect( QRectF( 0, m_image->height() + h_diff / 2, this->width(), h_diff / 2 ), QColor( 192, 195, 193 ) );
     }
 
     else
     {
-        QWidget::paintEvent( ev );
+        painter.drawImage( QPoint( 0, 0 ), ( *m_image ) );
+    }
+
+    painter.end();
+
+    if( !m_frame_shown )
+    {
+        m_frame_shown = true;
+        emit frameShown();
     }
 }
 
